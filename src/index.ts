c4cb00ee547948c4ec7158eb44c858e0c94ee952
Srdjan S <stankovic.srdjo@gmail.com>
2021-04-03 15:37:50 +0200

    Use github actions exec package to exec cmds

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,59 @@
-import {
-  login as dockerLogin,
-  build as dockerBuild,
-  push as dockerPush,
-  createImageTag,
-} from "./docker";
-
+import { exec } from "@actions/exec";
 import * as core from "@actions/core";
 
 type ActionInputs = {
-  serviceKeyJson: string,
-  imgRegistryID: string,
-  dockerContext: string,
-  dockerImageName: string,
-  dockerImageTag: string,
+  serviceKeyJson: string;
+  imgRegistryID: string;
+  dockerContext: string;
+  dockerImageName: string;
+  dockerImageTag: string;
 };
 
-const main = () => {
+const createImageTag = (
+  imgRegistryID: string,
+  name: string,
+  tag: string
+): string => {
+  return `cr.yandex/${imgRegistryID}/${name}:${tag}`;
+};
 
+const login = async (key: string) => {
+  const keyBuffer = Buffer.from(key);
+  await exec(
+    "docker",
+    ["login", "--username", "json_key", "--password-stdin", "cr.yandex"],
+    { input: keyBuffer }
+  );
+};
+
+const build = async (ctx: string, tag: string) => {
+  await exec(`docker build -t ${tag} ${ctx}`);
+};
+
+const push = async (tag: string) => {
+  await exec(`docker push ${tag}`);
+};
+
+const main = async () => {
   const inputs: ActionInputs = {
-    serviceKeyJson: core.getInput("YC_SERVICE_ACCOUNT_KEY_FILE", {required: true}),
-    imgRegistryID: core.getInput("YC_IMG_REGISTRY_ID", {required: true}),
+    serviceKeyJson: core.getInput("YC_SERVICE_ACCOUNT_KEY_FILE", {
+      required: true,
+    }),
+    imgRegistryID: core.getInput("YC_IMG_REGISTRY_ID", { required: true }),
     dockerContext: core.getInput("DOCKER_CONTEXT", { required: false }),
     dockerImageName: core.getInput("DOCKER_IMG_NAME", { required: true }),
     dockerImageTag: core.getInput("DOCKER_IMG_TAG", { required: false }),
   };
 
-  const imageTag = createImageTag(inputs.imgRegistryID, inputs.dockerImageName, inputs.dockerImageTag)
+  const imageTag = createImageTag(
+    inputs.imgRegistryID,
+    inputs.dockerImageName,
+    inputs.dockerImageTag
+  );
 
-  dockerLogin(inputs.serviceKeyJson);
-  dockerBuild(inputs.dockerContext, imageTag)
-  dockerPush(imageTag)
+  await login(inputs.serviceKeyJson);
+  await build(inputs.dockerContext, imageTag);
+  await push(imageTag);
 };
 
 main();
